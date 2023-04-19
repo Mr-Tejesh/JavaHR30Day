d94856c40b4d089e0a9666651579f3c012c80a03
Mr-Tejesh <114306366+Mr-Tejesh@users.noreply.github.com>
2023-04-19 18:40:15 +0530

    Add files via upload

---
+++ Day_0.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Day_0{
+		public static void main(String[] args)throws Exception{
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+				System.out.println("Please enter any String : ");
+				String input = bufferedReader.readLine().trim();
+				System.out.println("Hello, World.");
+				System.out.println(input);
+		}
+	
+}
